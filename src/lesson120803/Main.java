fa012d75e2cd3597551c9c5297904ae8af45cf27
unknown941 <unknown941@naver.com>
2023-03-02 13:15:19 +0900

    두 수의 차
    2023.03.02 풀이

---
+++ src/lesson120803/Main.java
@@ -0,0 +1,15 @@
+package lesson120803;
+
+import java.util.Arrays;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int[] case1 = {2, 3};
+		int[] case2 = {100, 2};
+		
+		System.out.println("CASE 1 : " + Solution.solution(case1[0], case1[1]));
+		System.out.println("CASE 2 : " + Solution.solution(case2[0], case2[1]));
+	}
+	
+}
